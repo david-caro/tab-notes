cdc898f4f946dda0d597fcbb2bf23e35b432b178
Dinika <dinikasaxenas@gmail.com>
2019-12-15 02:05:11 +0100

    fetch data on installation and store in chrome.storage.sync
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ color: '#3aa757' }, function() {
-    console.log('The color is green.');
+chrome.runtime.onInstalled.addListener(fetchAndStoreNotes);
+
+async function fetchAndStoreNotes() {
+  const notes = await fetch('https://tab-notes-dad94.firebaseio.com/notes.json');
+  const notesJson = await notes.json();
+  //TODO: New notes should perhaps be saved under `notes.difficult`
+  chrome.storage.sync.set({ notes: { difficult: notesJson } }, function () {
+    //TODO: Remove this function
+    chrome.storage.sync.get(null, function (data) {
+      console.log("sotrage data", data)
+    })
   });
-});
+}
